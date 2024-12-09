02de6afbc63d3c5a316e81637ee7b9ca75180047
Serhat Sağlam <serhatsaglamb@gmail.com>
2024-12-09 12:37:45 +0300

    little update

--- project/Server/obj/Debug/net6.0/project.Server.AssemblyInfo.cs
+++ project/Server/obj/Debug/net6.0/project.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffbf07b5797df018821d40cdc0b3575dc9610ac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0de8bf6bab454e332d5487e2376b9f90c46d8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("project.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
